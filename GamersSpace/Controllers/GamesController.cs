ba7a4cd17b77883aba58b73d85015a33a6b381ad
Roberto Solares <betosolaresgar@gmail.com>
2019-02-07 14:31:03 -0800

    Add the find game feature

--- GamersSpace/Controllers/GamesController.cs
+++ GamersSpace/Controllers/GamesController.cs
@@ -8,13 +8,6 @@ namespace GamersSpace.Controllers {
 
         // Create the list of games
         private List<Game> GameList() {
-            /**Game[] gameList = new Game[5];
-            gameList[0] = new Game() { id = 1, name = "Super Meat Boy", genre = "Platform", publisher = "Team Meat", rating = 9.0, release = 2010 };
-            gameList[1] = new Game() { id = 2, name = "Gish", genre = "Side-scrolling, Platform", publisher = "Chronic Logic", rating = 7.2, release = 2004 };
-            gameList[2] = new Game() { id = 3, name = "Bastion", genre = "Action", publisher = "Supergiant Games", rating = 8.6, release = 2011 };
-            gameList[3] = new Game() { id = 4, name = "Hyper Light Drifter", genre = "Adventure", publisher = "Heart Machine", rating = 8.8, release = 2016 };
-            gameList[4] = new Game() { id = 5, name = "Gone Home", genre = "Adventure", publisher = "The Fullbright Company", rating = 7.7, release = 2013 };
-    **/
             List<Game> games = new List<Game>() {
                 new Game() { id = 1, name = "Super Meat Boy", genre = "Platform", publisher = "Team Meat", rating = 9.0, release = 2010 },
                 new Game() { id = 2, name = "Gish", genre = "Side-scrolling, Platform", publisher = "Chronic Logic", rating = 7.2, release = 2004 },
@@ -25,11 +18,31 @@ namespace GamersSpace.Controllers {
             return games;
         }
 
-        // Return the view 
+        // Return the main view 
         public ActionResult Index() {
-            ViewBag.Games = GameList();
             return View();
         }
+
+	    /**
+         * @desc: Check if there is a game with the same id, and return the correct view.
+         * @param: string inputValue - The id to match.
+         * @return: View - The view to show.
+        **/
+	    public ActionResult GameInfo(string inputValue) {
+            if (int.TryParse(inputValue, out int number)) {
+                if (number <= GameList().Count) {
+                    return View(GameList()[number - 1]);
+                }
+                else {
+                    TempData["state"] = "incorrectID";
+                    return RedirectToAction("Index");
+                }
+            }
+            else {
+                TempData["state"] = "noNumber";
+                return RedirectToAction("Index");
+            }
+	    }
 
     }
 
--- GamersSpace/Scripts/functions.js
+++ GamersSpace/Scripts/functions.js
@@ -1,15 +1,1 @@
-﻿// Find game information
-function GameInfo(gameList) {
-    inputValue = document.getElementById('inputText').value;
-    if (!isNaN(inputValue)) {
-        if (inputValue <= gameList.length) {
-            alert(gameList[inputValue - 1].name)
-        }
-        else {
-            alert("No existe ese valor.")
-        }
-    }
-    else {
-        alert("Por favor inserte un numero.")
-    }    
-}
+﻿
