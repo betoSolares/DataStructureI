0df26cc4d0c40d938e317b6d19ecd913d600bcf1
Roberto Solares <betosolaresgar@gmail.com>
2019-02-09 10:31:29 -0800

    Add the message

--- GamersSpace/App_Start/BundleConfig.cs
+++ GamersSpace/App_Start/BundleConfig.cs
@@ -5,9 +5,6 @@ namespace GamersSpace {
     public class BundleConfig {
 
         public static void RegisterBundles(BundleCollection bundles) {
-            // JavaScript Bundle
-            bundles.Add(new ScriptBundle("~/bundles/script").Include(
-                "~/Scripts/functions.js"));
             // CSS Bundle
             bundles.Add(new StyleBundle("~/bundles/style").Include(
                 "~/Style/normalize.css",
