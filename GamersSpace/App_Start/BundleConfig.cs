1217b6dcd51277fc63486e94773552ac86aa998a
Roberto Solares <betosolaresgar@gmail.com>
2019-02-09 08:57:31 -0800

    Add the css using Skeleton

--- GamersSpace/App_Start/BundleConfig.cs
+++ GamersSpace/App_Start/BundleConfig.cs
@@ -8,6 +8,11 @@ namespace GamersSpace {
             // JavaScript Bundle
             bundles.Add(new ScriptBundle("~/bundles/script").Include(
                 "~/Scripts/functions.js"));
+            // CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                "~/Style/normalize.css",
+                "~/Style/skeleton.css",
+                "~/Style/site.css"));
         }
 
     }
