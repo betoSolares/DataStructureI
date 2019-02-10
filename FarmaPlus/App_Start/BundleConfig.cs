9069968402f1d54e5574d978f729a832701cb975
Roberto Solares <betosolaresgar@gmail.com>
2019-02-10 09:45:34 -0800

    Add the css bundle

---
+++ FarmaPlus/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace FarmaPlus {
+
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+            // CSS bundle
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                "~/Style/normalize.css",
+                "~/Style/skeleton.css",
+                "~/Style/site.css"));
+        }
+
+    }
+
+}
--- FarmaPlus/Global.asax.cs
+++ FarmaPlus/Global.asax.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace FarmaPlus
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+namespace FarmaPlus {
+
+    public class MvcApplication : System.Web.HttpApplication {
+
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
     }
+
 }
