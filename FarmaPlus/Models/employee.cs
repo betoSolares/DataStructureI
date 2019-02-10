cef228aab4d301d3e0e50ef613f46c6db2d34fc3
Roberto Solares <betosolaresgar@gmail.com>
2019-02-10 07:48:42 -0800

    Add the model for the employees

---
+++ FarmaPlus/Models/employee.cs
@@ -0,0 +1,13 @@
+﻿namespace FarmaPlus.Models {
+
+    public class employee {
+
+        string name { get; set; }
+        int codigo { get; set; }
+        int hoursWorked { get; set; }
+        bool inOffice { get; set; }
+        double salary { get; set; }
+
+    }
+
+}
