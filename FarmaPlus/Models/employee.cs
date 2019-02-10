22cfd31c9d0072a5a72dfbb7f901c9df7efa4d09
Roberto Solares <betosolaresgar@gmail.com>
2019-02-10 09:59:49 -0800

    Add the employees view

--- FarmaPlus/Models/employee.cs
+++ FarmaPlus/Models/employee.cs
@@ -3,7 +3,7 @@
     public class employee {
 
         string name { get; set; }
-        int codigo { get; set; }
+        int id { get; set; }
         int hoursWorked { get; set; }
         bool inOffice { get; set; }
         double salary { get; set; }
