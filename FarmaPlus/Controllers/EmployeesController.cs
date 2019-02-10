caf7e928c14c068376c2ab4d6cb95cc6600e4b63
Roberto Solares <betosolaresgar@gmail.com>
2019-02-10 08:04:54 -0800

    Create the controller

---
+++ FarmaPlus/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace FarmaPlus.Controllers {
+
+    public class EmployeesController : Controller {
+
+        // Return the form for insert a new employee
+        [HttpGet]
+        public ActionResult Employees() {
+            return View();
+        }
+
+    }
+
+}
