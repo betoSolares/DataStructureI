3bf36713fc73aeba88afc6d210b4dabd2cadafa0
Roberto Solares <betosolaresgar@gmail.com>
2019-02-10 09:21:40 -0800

    Add a new route

--- FarmaPlus/App_Start/RouteConfig.cs
+++ FarmaPlus/App_Start/RouteConfig.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace FarmaPlus
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+namespace FarmaPlus {
 
+    public class RouteConfig {
+
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
-                name: "Default",
+                name: "EntryPoint",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employees", action = "Employees", id = UrlParameter.Optional }
             );
         }
+
     }
+
 }
