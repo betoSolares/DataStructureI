6517fe000cdc16340a2071b85edc975021fee2d9
Roberto Solares <betosolaresgar@gmail.com>
2019-03-10 11:13:30 -0700

    Create the tree

--- Health/Controllers/InventoryController.cs
+++ Health/Controllers/InventoryController.cs
@@ -1,9 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using DataStructures;
+using Health.Models;
+using System.Web.Mvc;
 
 namespace Health.Controllers {
 
     public class InventoryController : Controller {
-        
+
+        // Tree for the meds
+        private static BST<Meds, string> tree = new BST<Meds, string>();
+
+        // Return the main view
         public ActionResult InventoryLoad() {
             return View();
         }
