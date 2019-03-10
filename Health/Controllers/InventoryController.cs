8de7efcf023435ac5c5d721147a7fadf6b6ee137
Roberto Solares <betosolaresgar@gmail.com>
2019-03-10 11:02:08 -0700

    Add a new controller

---
+++ Health/Controllers/InventoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Health.Controllers {
+
+    public class InventoryController : Controller {
+        
+        public ActionResult InventoryLoad() {
+            return View();
+        }
+
+    }
+
+}
