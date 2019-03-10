58bd506d7f4e4ab70f6fd52e651bb3ff9de9b6b4
Roberto Solares <betosolaresgar@gmail.com>
2019-03-10 11:49:17 -0700

    Add the bundles config

---
+++ Health/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace Health {
+
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+            // CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                "~/Styles/normalize.css",
+                "~/Styles/skeleton.css",
+                "~/Styles/site.css"));
+            // JS Bundle
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                "~/Scripts/site.js"));
+        }
+        
+    }
+
+}
