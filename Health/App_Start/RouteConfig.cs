b3b648ae3e4186c943f91b7fe2ee93af7fbe46b5
Roberto Solares <betosolaresgar@gmail.com>
2019-03-10 10:38:29 -0700

    Add a new route

--- Health/App_Start/RouteConfig.cs
+++ Health/App_Start/RouteConfig.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Health
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+namespace Health {
 
+    public class RouteConfig {
+
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
-                name: "Default",
+                name: "EntryPoint",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Inventory", action = "LoadInventory", id = UrlParameter.Optional });
         }
+
     }
+
 }
