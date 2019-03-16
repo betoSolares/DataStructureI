3c094fcafed231c98604982717c3e8ab333a2e18
Roberto Solares <betosolaresgar@gmail.com>
2019-03-16 13:25:04 -0700

    Add the file chooser

--- Health/Scripts/site.js
+++ Health/Scripts/site.js
@@ -1,6 +1,9 @@
-﻿$(document).ready(function () {
+﻿// Onload fuctions
+$(document).ready(function () {
     if ($('body').is('.InventoryLoad')) {
         if ($('#empty').val() == 'empty') {
+            $fiel = $('<input type="file" name="PathFile" class="input-file" id="fileChooser" />')
+            $fiel.appendTo($('#form'))
             $button = $('<input type="submit" class="button-primary btn" name="formAction" value="Cargar Inventario" />')
             $button.appendTo($('#form'))
         } else {
