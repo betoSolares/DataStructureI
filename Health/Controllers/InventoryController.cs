dabc8328f5e09a0a456510b63fdf8072bfddc894
Roberto Solares <betosolaresgar@gmail.com>
2019-03-21 17:37:38 -0700

    Finish the load of the inventory

--- Health/Controllers/InventoryController.cs
+++ Health/Controllers/InventoryController.cs
@@ -1,6 +1,8 @@
 ﻿using DataStructures;
 using Health.Models;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Health.Controllers {
@@ -13,12 +15,67 @@ namespace Health.Controllers {
         // Return the main view
         [HttpGet]
         public ActionResult InventoryLoad() {
-            if (tree.IsEmpty()) {
+            if (tree.IsEmpty())
                 ViewBag.Empty = "empty";
-            } else {
+            else
                 ViewBag.Empty = "noEmpty";
+            return View();
+        }
+
+        // Load or download the file
+        [HttpPost]
+        public ActionResult InventoryLoad(HttpPostedFileBase PathFile, string formAction) {
+            switch (formAction) {
+                case "Cargar Inventario":
+                    if (LoadFile(PathFile))
+                        TempData["state"] = "success";
+                    else
+                        TempData["state"] = "failed";
+                    if (tree.IsEmpty())
+                        ViewBag.Empty = "empty";
+                    else
+                        ViewBag.Empty = "noEmpty";
+                    break;
+                case "Descargar Inventario":
+                    break;
             }
             return View();
+        }
+
+
+        /**
+         * @desc: Verify if there is a file and load to the tree the elements.
+         * @param: HttpPostedFileBase fileUpload - the file to upload.
+         * @return: bool - True if there is a file, else false.
+        **/
+        private bool LoadFile(HttpPostedFileBase fileUpload) {
+            string path = string.Empty;
+            // Check if the fileUpload is not empty
+            if(fileUpload != null) {
+                // Store the file
+                string fileName = Path.GetFileName(fileUpload.FileName);
+                path = Path.Combine(Server.MapPath("~/App_Data/Files"), fileName);
+                fileUpload.SaveAs(path);
+                // Read the file and split each element
+                string file = System.IO.File.ReadAllText(path);
+                foreach(string line in file.Split('\n')) {
+                    if (!string.IsNullOrEmpty(line)) {
+                        string[] items = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                        // Create a new object and insert it into the tree
+                        Meds newMed = new Meds() {
+                            id = int.Parse(items[0]),
+                            name = items[1],
+                            description = items[2],
+                            production = items[3],
+                            price = float.Parse(items[4].Trim('$')),
+                            stock = int.Parse(items[5])
+                        };
+                        tree.Insert(newMed.name, newMed);
+                    }
+                }
+                return true;
+            }
+            return false;
         }
 
     }
--- Health/Scripts/site.js
+++ Health/Scripts/site.js
@@ -2,14 +2,14 @@
 $(document).ready(function () {
     if ($('body').is('.InventoryLoad')) {
         if ($('#empty').val() == 'empty') {
-            $input = $('<input type="file" name="PathFile" class="input-file" id="fileChooser" />')
+            $input = $('<input type="file" name="PathFile" class="input-file" id="PathFile" />')
             $input.appendTo($('#form'))
-            $file = $('<label for="fileChooser" id="file" class="file">Select a file</label>')
+            $file = $('<label for="PathFile" class="file">Select a file</label>')
             $file.appendTo($('#form'))
             $button = $('<input type="submit" class="button-primary btn" name="formAction" value="Cargar Inventario" />')
             $button.appendTo($('#form'))
         } else {
-            $button = $('<input type="submit" class="button-primary btn" name="formAction" value="Cargar Inventario" />')
+            $button = $('<input type="submit" class="button-primary btn" name="formAction" value="Descargar Inventario" />')
             $button.appendTo($('#form'))
         }
     }
