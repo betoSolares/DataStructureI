a79ae5d7e8492e57faff77532d36a79c1472430c
Roberto Solares <betosolaresgar@gmail.com>
2019-04-12 15:24:37 -0700

    Change the default route

--- Album/App_Start/RouteConfig.cs
+++ Album/App_Start/RouteConfig.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Album
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+namespace Album {
 
+    public class RouteConfig {
+
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Album", action = "LoadFiles", id = UrlParameter.Optional });
         }
+
     }
+
 }
