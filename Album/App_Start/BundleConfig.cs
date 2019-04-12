1676a12dde88f5d4f31bd3b5d2fa9c85a373c229
Roberto Solares <betosolaresgar@gmail.com>
2019-04-12 13:29:48 -0700

    Create and add the css bundle

---
+++ Album/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace Album {
+
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+            // CSS bundle
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                                        "~/Styles/bulma.min.css"));
+        }
+
+    }
+
+}
