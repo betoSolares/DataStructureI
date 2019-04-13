e77742172b124a457faf66250c7ceefa8e710e36
Roberto Solares <betosolaresgar@gmail.com>
2019-04-13 10:19:58 -0700

    Create the LoadFiles view

--- Album/Controllers/AlbumController.cs
+++ Album/Controllers/AlbumController.cs
@@ -4,7 +4,10 @@ namespace Album.Controllers {
 
     public class AlbumController : Controller {
 
-
+        // Return the view to load the files
+        public ActionResult LoadFiles(){
+            return View();
+        }
         
     }
 
