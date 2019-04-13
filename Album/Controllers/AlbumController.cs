236a9160b3d65c7e4cb6218441b590594bbdc3e5
Roberto Solares <betosolaresgar@gmail.com>
2019-04-13 15:10:48 -0700

    Create the dictionaries

--- Album/Controllers/AlbumController.cs
+++ Album/Controllers/AlbumController.cs
@@ -1,8 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using Album.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Album.Controllers {
 
     public class AlbumController : Controller {
+
+        // Dictionaries for the album and the stickers
+        private static Dictionary<string, Player> album = new Dictionary<string, Player>();
+        private static Dictionary<Sticker, State> collection = new Dictionary<Sticker, State>();
 
         // Return the view to load the files
         public ActionResult LoadFiles(){
