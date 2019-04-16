48750e8947900e0254f27eeb6fd5c8e1330381f4
Roberto Solares <betosolaresgar@gmail.com>
2019-04-16 10:49:25 -0700

    Add the function to search the stickers

---
+++ Album/Scripts/site.js
@@ -0,0 +1,12 @@
+﻿// Make the search for the stickers
+$("#buttonSearch").click(function () {
+    if ($("input[name=type]:checked").val() == "Special") {
+        $.post("/Album/SearchSticker", { type: "Special" }, function (data) {
+            console.log(data)
+        })
+    } else {
+        $.post("/Album/SearchSticker", { search: $("#search").val(), type: "Team" }, function (data) {
+            console.log(data)
+        })
+    }
+})
